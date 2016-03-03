8ddb8305df2fb017cd5045119b159fb125d973d4
Robert Black <robertblack@Roberts-MacBook-Air.local>
2016-03-03 16:02:07 -0500

    media query added

---
+++ assets/js/game.js
@@ -0,0 +1,212 @@
+// each game board is divided into an array of answers. each answer is an array containing
+// the formal name, points, a bool to test if it has been answered before, and 
+// keywords that will lead to the answer. 
+var child = ["Children", 16, false, "kids", "kid", "children", "child", "daughter", "son"];
+var car = ["Car", 14, false, "vehicle", "car", "autmobile", "wheels"];
+var pet = ["Pet", 10, false, "dog", "cat", "pet"];
+var house = ["House", 10, false, "apartment", "house", "condo", "place", "room"];
+var comp = ["Computer", 9, false, "labtop", "desktop", "computer", "tablet"];
+var me = ["Myself", 8, false, "me", "i", "myself"];
+var cell = ["Cell Phone", 5, false, "phone", "mobile", "cell", "iphone", "android"];
+var spouse = ["Spouse", 4, false, "wife", "husband", "partner", "spouse"];
+var game10 = ["Tell me something of yours that you swear is possessed.", child, car, pet, house, comp, me, cell, spouse];
+var clothes = ["Old Clothes/Shoes", 38, false, "clothes", "shoes", "shirt", "pants", "shirts"];
+var pet2 = ["Pet/Animal", 8, false, "dog", "cat", "pet", "animal"];
+var trash = ["Trash", 7, false, "trash", "garbage", "waste"];
+var mistress = ["Mistress", 5, false, "mistress", "girl", "girlfriend", "side", "affair"];
+var mags = ["Magazines/Books", 5, false, "magazine", "magazines", "book", "books"];
+var adult = ["Adult Video", 4, false, "videos", "video", "adult", "porn", "porno"];
+var car2 = ["Car/Convertable", 4, false, "car", "convertable", "autmobile", "vehicle"];
+var tude = ["Bad Attitude", 4, false, "bad", "attitude", "temper", "mood"];
+var game9 = ["Name something a wife tells her husband to get rid of.", clothes, pet2, trash, mistress, mags, adult, car2, tude];
+var college = ["School/College", 56, false, "school", "college", "class"];
+var church = ["Church", 26, false, "church", "sunday", "mass", "temple"];
+var family = ["Family Function", 9, false, "family", "together", "party", "birthday", "vacation", "grandma's"];
+var doctor = ["Dentist/Doctor", 4, false, "dentist", "doctor"];
+var store = ["Grocery Store", 2, false, "grocery", "store", "market", "mart"];
+var game1 = ["Name someplace a teenager complains about having to go to.", college, church, family, doctor, store];
+var money = ["Money/Status", 57, false, "money", "status", "wealth"];
+var lust = ["Lust/Infatuation", 34, false, "lust", "infatuation", "attraction", "sex", "looks", "inpulse"];
+var child2 = ["Children/Pregnant", 5, false, "kids", "kid", "children", "child", "daughter", "son", "pregnant"];
+var expect = ["People Expect It", 2, false, "arranged", "told", "family", "expect", "expected", "said", "wanted"];
+var spite = ["To Spite Parents", 2, false, "spite", "not", "didn't"];
+var game2 = ["Love is the right reason to get married. Name one of the wrong reasons.", money, lust, child2, expect, spite];
+var cook = ["Cook for them", 47, false, "cook", "breakfast", "lunch", "dinner", "food"];
+var laundry = ["Laundry/Ironing", 39, false, "laundry", "iron", "ironing", "wash", "clothes"];
+var money2 = ["Loan Them Money", 7, false, "money", "cash", "borrow", "loan"];
+var shop = ["Shop for Them", 2, false, "shop", "shopping", "mall", "buy"];
+var ride = ["Give Them A Ride", 2, false, "drive", "ride", "take"];
+var sing = ["Sing for Them", 2, false, "sing", "sleep", "lullaby", "tuck"];
+var game3 = ["Name something grown men still ask their mommy to do for them.", cook, laundry, money2, shop, ride, sing];
+var movie = ["Watch TV/Movie", 77, false, "movie", "tv", "television", "netflix"];
+var vgames = ["Play Games", 10, false, "play", "games", "game", "xbox", "videogames"];
+var sleep = ["Sleep/Relax", 5, false, "sleep", "nap", "relax", "lay", "chill"];
+var cook2 = ["Cook/Eat Dinner", 3, false, "eat", "cook", "dinner", "lunch"];
+var love = ["Make Love", 2, false, "love", "sex", "whoopie", "bed"];
+var friends = ["Have Friends Over", 2, false, "friends", "party", "together", "over"];
+var game4 = ["What do people do at home on a Saturday night if they can't afford to go out?", movie, vgames, sleep, cook2, love, friends];
+var cry = ["Cry", 22, false, "cry", "sob", "weep", "upset"];
+var leave = ["Leave", 22, false, "leave", "move", "leaving"];
+var job = ["Look for a job", 15, false, "look", "job", "another", "find"];
+var tell = ["Tell them Off", 9, false, "yell", "tell", "off", "scream", "fuck", "say"];
+var desk = ["Clean out Desk", 9, false, "clean", "desk", "office"];
+var hit = ["Hit them/Revenge", 6, false, "hit", "punch", "kick", "revenge", "back"];
+var ask = ["Ask/Wonder Why", 4, false, "ask", "why", "wonder", "think"];
+var quit = ["Say 'I Quit'", 3, false, "quit"];
+var game5 = ["What might you do after your boss says, 'You're fired'?", cry, leave, job, tell, desk, hit, ask, quit];
+var pens = ["Pens/Pencils", 50, false, "pen", "pens", "pencil", "pencils", "writing", "utensils"];
+var paper = ["Paper", 20, false, "paper", "parchment", "sheet", "papers"];
+var stapler = ["Stapler", 6, false, "stapler", "staplers"];
+var clips = ["Paper Clips", 6, false, "clip", "clips", "pin"];
+var comp2 = ["Computer", 4, false, "computer", "tablet"];
+var tape = ["Tape", 4, false, "tape", "scotch", "duct"];
+var post = ["Post-its", 3, false, "post", "sticky", "notes", "post-it", "post-its"];
+var tp = ["Toilet Paper", 3, false, "tp", "toilet"];
+var game6 = ["Name something people often steal from the office to use at home.", pens, paper, stapler, clips, comp2, tape, post, tp];
+var shoes3 = ["Shoes/Clothes", 17, false, "shoes", "clothes", "pants", "shirt", "heels"];
+var jewl = ["Jewelry/Watch", 13, false, "jewelry", "watch", "necklace", "wristlet", "bracelet", "gem", "ring"];
+var house3 = ["House", 10, false, "house", "place", "apartment", "condo", "room"];
+var car3 = ["Car", 9, false, "car", "vehicle", "automobile", "wheels"];
+var pet3 = ["Pet", 8, false, "pet", "dog", "cat", "bird", "turtle", "hampster"];
+var friends2 = ["Friendships", 8, false, "friend", "friends", "friendship", "friendships"];
+var child3 = ["Children", 5, false, "child", "children", "son", "daughter", "kid", "kids"];
+var job3 = ["Job", 4, false, "job", "jobs", "work"];
+var game7 = ["We surveyed 100 single women. Tell me something you have that lasted longer than most relationships.", shoes3, jewl, house3, car3, pet3, friends2, child3, job3];
+// then each gameboard is arranged into an array, making the 3D array of games
+var games = [game1, game5, game2, game3, game4, game6, game7, game9, game10];
+var gameNum = 0;
+var wrongAnswers = 0;
+$("#start").click(function() {
+	$("#start").attr("style", "display: none");
+	newBoard();
+});
+function guessAns(){
+	var ansbool = false;
+	var answerArr = ($("#input").val()).split(" ");
+	for(var k=0; k<answerArr.length; k++){
+		for(var i=1; i<games[gameNum].length; i++){
+			for(var j=3; j<games[gameNum][i].length; j++){
+				if(answerArr[k].toLowerCase() == games[gameNum][i][j] && games[gameNum][i][2] == false){
+					$("#sounds").html("<audio autoplay><source src='assets/audio/right.mp3' type='audio/mp3'></audio>");
+					$("#answer"+(i-1)).html("<div style='height: 50px; animation-name: flipIn; animation-duration: 0.5s; animation-timing-function: linear; background-image: url(assets/images/answer.jpg)'><p class='answerText'>"+games[gameNum][i][0]+"</p>"+"<p class='scoreText'>"+games[gameNum][i][1]+"</p></div>");
+					$("#clock").html("15");
+					var score = parseInt($("#score").html());
+					score += games[gameNum][i][1];
+					$("#score").html(score);
+					ansbool = true;
+					games[gameNum][i][2] = true;
+				};
+			};
+		};
+	};
+	if(ansbool == false){
+		$("#clock").html("15");
+		wrongAnswer();
+	};
+	ansbool = true;
+	for(i=1; i<games[gameNum].length; i++){
+		if(games[gameNum][i][2] == false){
+			ansbool = false;
+		};
+	};
+	if(ansbool == true){
+		$("#info-question").html("");
+		var newScore = parseInt($("#score").html());
+		newScore += 200;
+		$("#score").html(newScore);
+		$("#xes").html("");
+		endGame();
+	};
+	$("#input").val("");
+};
+$("#guess").click(function() {
+	guessAns();
+});
+document.onkeyup = function(e) {
+	if(e.keyCode == 13 && $("#input").val() != ""){
+		guessAns();
+	};
+};
+$("#next").click(function() {
+	$("#next").attr("style", "display: none");
+	newBoard();
+});
+$("#startOver").click(function() {
+	gameNum = 0;
+	$("#score").html("0");
+	$("#startOver").attr("style", "display: none");
+	for(var i=0; i<games.length; i++){
+		for(var j=1; j<games[i].length; j++){
+			games[i][j][2] = false;
+		};
+	};
+	newBoard();
+});
+function newBoard(){
+	for(var i=0; i<8; i++){
+		var gameLength = (games[gameNum].length) - 1;
+		if(i<gameLength){
+			$("#answer"+i).html("<div style='background-image: url(assets/images/unAnswered.jpg); height: 50px; width: 200px'><p class='answerNumber'>"+(i+1)+"</p></div>");
+		} else {
+			$("#answer"+i).html("<div style='background-image: url(assets/images/noAnswer.jpg); height: 50px; width: 200px'><p class='exEs'>X X X</p><div>");
+		};
+	};
+	$("#input-screen").attr("style", "visibility: hidden");
+	$("#info-question").attr("style", "visibility: initial");
+	$("#xes").html("");
+	$("#input").val("");
+	$("#info-question").html(games[gameNum][0]);
+	wrongAnswers = 0;
+	setTimeout(function(){
+	promptAnswer();
+	}, 5000);
+};
+function promptAnswer(){
+	$("#input-screen").attr("style", "visibility: initial");
+	$("#clock").attr("style", "visibility: initial");
+	$("#clock").html("15");
+	var timer = setInterval(function(){
+		var time = parseInt($("#clock").html());
+		time--;
+		if(time == 0){
+			wrongAnswer();
+			time = 15;
+		};
+		$("#clock").html(time);
+	}, 1000);
+}
+function wrongAnswer(){
+	$("#sounds").html("<audio autoplay><source src='assets/audio/wrong.mp3' type='audio/mp3'></audio>");
+	wrongAnswers++;
+	if(wrongAnswers == 3){
+		$("#info-question").html("");
+		$("#input").val("");
+		$("#xes").html("[X][X][X]");
+		for(var j=1; j<games[gameNum].length; j++){
+			$("#answer"+(j-1)).html("<div style='height: 50px; background-image: url(assets/images/answer.jpg)'><p class='answerText'>"+games[gameNum][j][0]+"</p><p class='scoreText'>"+games[gameNum][j][1]+"</p></div>");
+		};
+		endGame();
+		return;
+	}
+	var exes = "";
+	for(var i=0; i<wrongAnswers; i++){
+		exes += "[X]";
+	};
+	$("#xes").html(exes);
+	setTimeout(function(){
+		$("#xes").html("");
+	}, 3000);
+};
+function endGame(){
+	gameNum++;
+	$("#next").attr("style", "display: initial");
+	$("#input-screen").attr("style", "visibility: hidden");
+	$("#clock").attr("style", "visibility: hidden");
+	for(var i=0; i<999; i++){
+		clearInterval(i);
+	};
+	if(gameNum == games.length){
+		$("#next").attr("style", "display: none");
+		$("#startOver").attr("style", "display: initial");
+	};
+};
